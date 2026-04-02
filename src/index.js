9e68cd01f6ddb7444b9c33d2d33d03fc72baaf33
Tambeej <49399681+Tambeej@users.noreply.github.com>
2026-04-03 01:49:02 +0300

    fix: import tailwind CSS in index.js entry point

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import './styles/globals.css';
 import App from './App';
 import { register as registerServiceWorker } from './serviceWorkerRegistration';
